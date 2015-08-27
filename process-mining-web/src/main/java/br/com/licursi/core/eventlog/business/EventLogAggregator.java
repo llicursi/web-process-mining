3b201f9df8412f82b3ed50bdf03005483a239360
unknown <llicursi@gmail.com>
2015-08-26 23:04:40 -0300

    Fim

--- process-mining-web/src/main/java/br/com/licursi/core/eventlog/business/EventLogAggregator.java
+++ process-mining-web/src/main/java/br/com/licursi/core/eventlog/business/EventLogAggregator.java
@@ -5,7 +5,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
